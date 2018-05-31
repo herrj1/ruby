318f9622151699b696028af858fb39fe40784e74
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-05-30 21:52:51 -0400

    Added linkedlist implementation

---
+++ datastructures/LinkedList.rb
@@ -0,0 +1,28 @@
+class Node
+	attr_accessor :value, :next_node
+	
+	def initialize(value, next_node=nil)
+		@value = value
+		@next = next_node
+	end
+end
+
+class LinkedList
+	def initialize(value)
+		@head = Node.new(value)
+	end
+	
+	def add(value)
+		current = @head
+		while !current.next_node.nil?
+			current = current.next_node
+		end
+		current.next_node = Node.new(value)
+	end
+end
+
+#Example:
+
+numbers = LinkedList.new
+numbers.add(30)
+numbers.add(50)
