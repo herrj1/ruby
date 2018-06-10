0641d4991152c00b2d51e63bd9ec691d8cbbb5bd
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-09 20:09:04 -0400

    Added objects implementation

---
+++ handcode/Object.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/ruby -w
+
+#define a class
+class Box
+	@@brand = ''
+	#constructor method
+	def initialize(w,h)
+		@width, @height = w, h
+	end
+	
+	def getWidth
+		@width
+	end
+	
+	def getHeight
+		@height
+	end
+	
+	def setWidth=(value)
+		@width = value
+	end
+	
+	def setHeight=(value)
+		@height = value
+	end
+	
+	def setBrand=(value)
+		@@brand = value
+	end
+	
+	def getBrand
+		@@brand
+	end
+	
+	def printDetails()
+		#Box.setBrand()
+		puts "Box brand is: #@@brand"
+		puts "Box size is: h #@height X W #@width"
+		
+	end
+end
+
+
+box1 = Box.new(10, 20)
+box1.setBrand = 'Sony'
+box1.printDetails()
