2edf5c76bfe52b5267da148edefce2477658b6f3
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-09 20:10:21 -0400

    Added map array implementation

---
+++ handcode/mappingarray.rb
@@ -0,0 +1,13 @@
+def recur_map(array, f)
+	if array.empty?
+		[]
+	else
+		head, tail = array.head_tail
+		[f.(head)] + recur_map(tail, f)
+	end
+end
+
+#example
+recur_map([1,2,3,4,5,6], ->(elem){
+	elem * elem
+})
