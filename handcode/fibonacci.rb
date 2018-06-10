17a4813f205f6324a226df6d2b5682bd229cae81
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-06-09 20:08:34 -0400

    Added fibonacci implementation

---
+++ handcode/fibonacci.rb
@@ -0,0 +1,15 @@
+def recur_fib(n)
+	acc = [0,1]
+	
+	f = ->(acc) {
+		if acc.size = n
+			acc
+		else
+			cur, last = acc.last(2)
+			f.(acc + [cur + last])
+		end
+	}
+	f.(acc)
+end
+
+recur_fib(12)
